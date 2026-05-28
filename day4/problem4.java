ad7f775865093673650aa3829f5260286398f2e0
srii1412 <srijayadevinatesan@gmail.com>
2026-05-28 19:23:05 +0530

    Create problem4.java

---
+++ day4/problem4.java
@@ -0,0 +1,22 @@
+// Fabonicci series 
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number: ");
+        int n = sc.nextInt();
+
+        int a = 0, b = 1;
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(a + " ");
+
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+    }
+}
